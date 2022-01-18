19fd39a1ddcc784827cc297b517690b866ade321
Raphaelle Tseng <raphaelletseng@gmail.com>
2022-01-18 14:25:43 -0500

    challenge 7

---
+++ 7_10001stPrime.py
@@ -0,0 +1,22 @@
+#By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that the 6th prime is 13.
+
+#What is the 10 001st prime number?
+
+def genPrime(index):
+    primes = [2]
+    start = 3
+    while(len(primes) < index):
+        track = True
+        for i in primes:
+            if (start%i == 0): 
+                track = False
+                #start is not a prime
+        if (track == True): primes.append(start)
+        start= start + 1     
+    #return primes       
+    return primes[len(primes)-1]
+
+print(genPrime(10001))
+    
+#ANS: 104743
+
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # project-euler
 A space to put python solutions to Project Euler
 
+https://projecteuler.net/
 
 
