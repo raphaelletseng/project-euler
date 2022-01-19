3e4c0fee85535652dd23c4a4b15d8a88398cc6d3
Raphaelle Tseng <raphaelletseng@gmail.com>
2022-01-19 09:37:08 -0500

    challenge 9

---
+++ 9_specialPythagTriplet.py
@@ -0,0 +1,22 @@
+#A Pythagorean triplet is a set of three natural numbers, a < b < c, for which
+#a^2 + b^2 = c^2
+
+# find the pythag triplet where a + b + c = 1000
+
+def isPythagTrip(a, b, c):
+    if (((a*a)+(b*b)) == (c*c)): return True
+
+
+def equals1000 (a,b,c):
+    if ((a + b + c) == 1000) : return True
+
+
+for a in range(150, 800):
+    for b in range (150, 800):
+        for c in range (150, 800):
+            if( isPythagTrip (a,b,c)):
+                if (equals1000 (a,b,c)):
+                    print(a*b*c)
+
+
+#ANS: 31875000
