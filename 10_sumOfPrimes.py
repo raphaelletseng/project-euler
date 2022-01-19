1d357494eb8de53367b55cd727fa6b0daf5659c8
Raphaelle Tseng <raphaelletseng@gmail.com>
2022-01-19 17:56:26 -0500

    challenge 13

--- 10_sumOfPrimes.py
+++ 10_sumOfPrimes.py
@@ -1,6 +1,8 @@
 #The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
 
 #Find the sum of all the primes below two million.
+import math
+from tkinter import N
 
 def genPrime(max):
     primes = [2]
@@ -20,7 +22,44 @@ def genPrime(max):
     return sum   
        
     
-print(genPrime(2000000))
+#print(genPrime(2000000))
     
 #ANS: 142913828922
 # But omg find a faster way to do this Raph
+
+#Looping Algo to find primes
+def isPrime(n):
+    if (n == 1):
+        return False
+    else:
+        if (n < 4): return True
+        else:
+            if (n%2 == 0):
+                return False
+            else:
+                if (n<9): return True
+                else:
+                    if (n%3 == 0): return False
+                    else:
+                        r = math.floor(math.sqrt(n))
+                        f = 5
+                        while (f <= r):
+                            if (n%f==0): 
+                                return False
+                            if(n%(f+2)==0): return False
+                            f = f+6
+                    return True
+
+limit = 2000000
+sum = 5
+n = 5
+while (n<= limit):
+    if isPrime(n): sum = sum+n 
+    n = n+2
+    if (n<= limit and isPrime(n)): sum = sum +n 
+    n = n+4
+
+print(sum)
+
+#wow this was SO much better
+
