95fea5eca67c6a9acf227d7261d0696b16521d69
Raphaelle Tseng <raphaelletseng@gmail.com>
2022-01-19 20:41:03 -0500

    change test

--- test_files.py
+++ test_files.py
@@ -1,11 +1,11 @@
-from solutions import one
+from solutions.one import solution
 
-SOLUTIONS_DICT = {
-    one: 233168
-}
+#SOLUTIONS_DICT = {
+#    one: 233168
+#}
 
 
 def test_all():
-
-    for key, val in SOLUTIONS_DICT.items():
-        assert key.solution == val
+    assert solution == 233168
+    #for key, val in SOLUTIONS_DICT.items():
+     #   assert key.solution == val
